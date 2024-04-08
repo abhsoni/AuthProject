5386121e32b4d75ac2fe6a081290e83b0ef6575f
abhsoni <abhisheksoni11999@gmail.com>
2024-04-09 03:52:14 +0530

    First Draft- Authentication of category page-6

--- src/app/categories-page/page.tsx
+++ src/app/categories-page/page.tsx
@@ -13,7 +13,7 @@ export default function CategoriesPage() {
         }
         router.push("/");
         router.refresh();
-        return console.log(localStorage.getItem("userId"));
+        return ;
     }
     return (
         <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
